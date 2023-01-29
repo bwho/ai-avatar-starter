14599ba0fe8f7ec35bedbe07b6fc6b5ac3922e45
bwho <barrett.olafson@gmail.com>
2023-01-29 15:55:21 -0500

    Added initial code, prior to working on API updates

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,64 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState(''); 
+  
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const generateAction = async () => {
+    console.log('Generating...');
+
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+  
+    const data = await response.json();
+
+  if (response.status === 503) {
+    console.log('Model is loading still :(.')
+    return;
+  }
+
+  if (!response.ok) {
+    console.log(`Error: ${data.error}`);
+    return;
+  }
+
+  setImg(data.image);
+  }
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI-powered headshot generator | buildspace</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>AI-powered headshot generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Turn me into anyone, just use "bwho" in the prompt</h2>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={generateAction} >
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
